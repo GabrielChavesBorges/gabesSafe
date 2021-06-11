a93213dca96c804fe2c51afd84621c2723229f94
GabrielChavesBorges <gabriel.chaves.borges@gmail.com>
2021-06-11 11:33:38 +0900

    Add package setup.

---
+++ app.js
@@ -0,0 +1,7 @@
+// Package setup: --------------------------------------------------------------
+
+import dotEnv from "dotenv";
+dotEnv.config();
+
+import React from "react";
+import Mongoose from "mongoose";
