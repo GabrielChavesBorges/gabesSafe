90a2deae56cb2b92af7f67830bd6103fc31ae6cb
GabrielChavesBorges <gabriel.chaves.borges@gmail.com>
2021-06-11 11:35:17 +0900

    Add database setup.

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ dotEnv.config();
 
 import React from "react";
 import Mongoose from "mongoose";
+
+// Database setup: -------------------------------------------------------------
+
+const databaseProtocol  = "mongodb+srv://"
+const databaseUser      = process.env.DB_USER + ":";
+const databasePassword  = process.env.DB_PASSWORD;
+const databaseName      = process.env.DB_NAME;
+const databaseLocation  = process.env.DB_LOCATION;
+
+mongoose.connect(
+  databaseProtocol  +
+  databaseUser      +
+  databasePassword  +
+  databaseLocation  +
+  databaseName
+)
