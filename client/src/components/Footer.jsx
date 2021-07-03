0cda51ae3c9ab4e4a4769633f658af39b4c1812f
GabrielChavesBorges <gabriel.chaves.borges@gmail.com>
2021-07-03 12:28:38 +0900

    Improved footer style.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,10 +4,37 @@ function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer>
-      <p>
-        {`Creative Commons ⓒ ${year}`}
-      </p>
+    <footer className="footer">
+      <div>
+        {`(CC) ${year} Gabriel Borges`}
+      </div>
+      <div>
+        Photo by
+        <a href="https://unsplash.com/@jdent?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          {' Jason Dent'}
+        </a>
+        {' on'}
+        <a href="https://unsplash.com/s/photos/safe?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          {' Unsplash'}
+        </a>
+      </div>
+
+      <div>
+        Icons made by
+        <a
+          href="https://www.flaticon.com/authors/smashicons"
+          title="Smashicons"
+        >
+          {' Smashicons'}
+        </a>
+        {' from'}
+        <a
+          href="https://www.flaticon.com/"
+          title="Flaticon"
+        >
+          {' www.flaticon.com'}
+        </a>
+      </div>
     </footer>
   );
 }
